69b4f130f316ec4a034556c670a3458919c4264c
Tim Kurvers <tim@moonsphere.net>
2015-11-06 12:37:35 +0100

    Enforce indentation of switch/case statements

--- fixtures/react/index.jsx
+++ fixtures/react/index.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 class Configuration extends React.Component {
 
-  constructor() {
+  constructor(props) {
     this.state = { enabled: true };
+
+    switch (props.flag) {
+      case 0:
+        this.state.enabled = false;
+        break;
+      default:
+    }
   }
 
   render() {
